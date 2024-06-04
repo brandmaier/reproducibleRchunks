5824d172509023138084df147765ccd4912a02d6
Andreas Brandmaier <andy@brandmaier.de>
2024-06-04 17:13:03 +0200

    reformatting

--- R/save_repro_data.R
+++ R/save_repro_data.R
@@ -1,7 +1,15 @@
-default_filetype <- function() getOption("reproducibleRchunks.filetype", "json")
-default_digits <- function() getOption("reproducibleRchunks.digits", 8)
-default_hashing <- function() getOption("reproducibleRchunks.hashing", TRUE)
-default_hashing_algorithm <- function() getOption("reproducibleRchunks.hashing_algorithm", "sha256")
+default_filetype <-
+  function()
+    getOption("reproducibleRchunks.filetype", "json")
+default_digits <-
+  function()
+    getOption("reproducibleRchunks.digits", 8)
+default_hashing <-
+  function()
+    getOption("reproducibleRchunks.hashing", TRUE)
+default_hashing_algorithm <-
+  function()
+    getOption("reproducibleRchunks.hashing_algorithm", "sha256")
 
 #' @title Storing reproducibility data
 #' @param x{description}
@@ -12,51 +20,66 @@ default_hashing_algorithm <- function() getOption("reproducibleRchunks.hashing_a
 #' @export
 save_repro_data <- function(x,
                             filename,
-                            filetype=default_filetype(),
+                            filetype = default_filetype(),
                             envir = NULL) {
-  if (is.null(envir)) envir = sys.parent()
+  # search the calling frame for definitions, ignore
+  # local definitions such as x, filename, filetype
+  if (is.null(envir))
+    envir = sys.parent()
 
-  if (tolower(filetype)=="json") {
-
+  if (tolower(filetype) == "json") {
     exist_all <- all(sapply(x, function(x) {
-      if (is.null(envir)) exists(x)
-      else exists(x, envir=envir)
-      } ))
-    if (!exist_all){
+      if (is.null(envir))
+        exists(x)
+      else
+        exists(x, envir = envir)
+    }))
+    if (!exist_all) {
       missing_obj_names <- names(which(sapply(x, function(x) {
-        if (is.null(envir)) !exists(x) else
-        !exists(x, envir=envir)
+        if (is.null(envir))
+          ! exists(x)
+        else
+          ! exists(x, envir = envir)
 
-        })))
-      stop("Some objects to be saved do not exist: ",paste0(missing_obj_names))
+      })))
+      stop("Some objects to be saved do not exist: ",
+           paste0(missing_obj_names))
     }
 
     if (is.null(envir))
-     named_list <- lapply(x, function(x){ get(x)})
+      named_list <- lapply(x, function(x) {
+        get(x)
+      })
     else
-      named_list <- lapply(x, function(x){ get(x,envir=envir)})
+      named_list <- lapply(x, function(x) {
+        get(x, envir = envir)
+      })
 
     names(named_list) <- x
     if (isTRUE(default_hashing())) {
       named_list <- lapply(named_list, hash)
     }
 
-    payload <- list(metadata = list(
-      hashing = default_hashing(),
-      hashing_algorithm = default_hashing_algorithm(),
-      hashing_package = "digest",
-      hashing_package_version = packageVersion("digest")
-    ),
-         data = named_list)
+    payload <- list(
+      metadata = list(
+        hashing = default_hashing(),
+        hashing_algorithm = default_hashing_algorithm(),
+        hashing_package = "digest",
+        hashing_package_version = packageVersion("digest"),
+        digits = default_digits()
+      ),
+      data = named_list
+    )
 
-    json_data <- jsonlite::serializeJSON(payload, pretty=TRUE, digits = default_digits())
+    json_data <- jsonlite::serializeJSON(payload,
+                                         pretty = TRUE,
+                                         digits = default_digits())
     con <- file(filename)
     writeLines(json_data, con)
     close(con)
-  } else if (tolower(filetype)=="rda") {
-    save(list=x, file = filename)
+  } else if (tolower(filetype) == "rda") {
+    save(list = x, file = filename)
   } else {
     stop("Unknown filetype. Cannot save reproducibility data.")
   }
 }
-
