0d4d49d534e36e2815e662289b75b9d2f3289610
Andreas Brandmaier <andy@brandmaier.de>
2024-06-06 16:32:22 +0200

    reformatting for pretty code

--- R/reproducibleR.R
+++ R/reproducibleR.R
@@ -15,11 +15,12 @@
 #' @export
 #'
 reproducibleR <- function(options) {
-  path <- dirname(knitr::current_input(dir=TRUE))
+  path <- dirname(knitr::current_input(dir = TRUE))
   this_filename <- knitr::current_input()
   # determine requested output format
   output_format <- knitr::pandoc_to()
-  if (is.null(output_format)) output_format <- "undef"
+  if (is.null(output_format))
+    output_format <- "undef"
   # initialize empty output vector
   output = c()
   # error counter set to zero
@@ -27,49 +28,78 @@ reproducibleR <- function(options) {
   # get label of chunk, must be non-empty
   label <- options$label
   stopifnot(!is.null(label))
-  stopifnot(label!="")
+  stopifnot(label != "")
   # get code as a single string
   code <- paste(options$code, collapse = "\n")
   # build code fingerprint
-  code_fingerprint <- digest::digest(code, algo="sha256")
+  code_fingerprint <- digest::digest(code, algo = "sha256")
   # create an environment for the current reproduction attempt
   current_env <- knitr::knit_global()#globalenv()
   existing_var_names <- ls(current_env)
   # evaluate code
-  result <- eval(parse(text=code), envir = current_env)
+  result <- eval(parse(text = code), envir = current_env)
   # get all defined variables
   current_vars <- ls(current_env)
   # remove those that existed already (from previous chunks)
-  current_vars <- current_vars[sapply(current_vars,function(x){!x %in% existing_var_names})]
+  current_vars <-
+    current_vars[sapply(current_vars, function(x) {
+      !x %in% existing_var_names
+    })]
   # set filetype
   # get file with repro values
-  filename <- paste0(path,"/",".repro_",this_filename,"_",label,".",default_filetype(),collapse="")
-  cat("Filename: ", filename,"\n")
+  filename <-
+    paste0(path,
+           "/",
+           ".repro_",
+           this_filename,
+           "_",
+           label,
+           ".",
+           default_filetype(),
+           collapse = "")
+
   # does the file exist?
   if (!file.exists(filename)) {
-    output <- c(output, "**Creating reproduction file**\n This seems to be the first run of the R Markdown file including reproducible chunks.\nStoring reproducibility information for variables:\n ",paste0("- ",current_vars,collapse="\n"))
+    output <-
+      c(
+        output,
+        "**Creating reproduction file**\n This seems to be the first run of the R Markdown file including reproducible chunks.\nStoring reproducibility information for variables:\n ",
+        paste0("- ", current_vars, collapse = "\n")
+      )
     # are there any variables defined at all?
-    if (length(current_vars)==0) {
-      warning("No variables were created. No reproduction report possible for current chunk ",label,".")
+    if (length(current_vars) == 0) {
+      warning(
+        "No variables were created. No reproduction report possible for current chunk ",
+        label,
+        "."
+      )
     } else {
       # save all defined files
-      save_repro_data(current_vars, filename, filetype = "json",
-                      envir = current_env,
-                      extra=list(code_fingerprint=code_fingerprint))
+      save_repro_data(
+        current_vars,
+        filename,
+        filetype = "json",
+        envir = current_env,
+        extra = list(code_fingerprint = code_fingerprint)
+      )
     }
 
   } else {
     # restore original results
     repro_env <- new.env()
     meta_data <- load_repro_data(filename,
-                                 envir=repro_env,
-                                 filetype="json")
+                                 envir = repro_env,
+                                 filetype = "json")
 
     # compare code fingerprint (if exists)
-    if (hasName(meta_data,"code_fingerprint")) {
-
-      if(!identical(meta_data$code_fingerprint, code_fingerprint)) {
-        output <- c(warning_symbol(output_format)," Warning: Current code chunk fingerprint and stored code chunk fingerprint mismatch. Likely, the code chunk was modified after reproduction data was stored the first time.\n", output)
+    if (hasName(meta_data, "code_fingerprint")) {
+      if (!identical(meta_data$code_fingerprint, code_fingerprint)) {
+        output <-
+          c(
+            warning_symbol(output_format),
+            " Warning: Current code chunk fingerprint and stored code chunk fingerprint mismatch. Likely, the code chunk was modified after reproduction data was stored the first time.\n",
+            output
+          )
       }
 
     }
@@ -82,8 +112,15 @@ reproducibleR <- function(options) {
     for (var in ls(repro_env)) {
       original_value <- get(var, envir = repro_env)
 
-      if (!exists(var, envir=current_env)) {
-        output <- c(output, fail_symbol(), "Reproduction error! Variable ",var, " was not defined in this chunk!")
+      if (!exists(var, envir = current_env)) {
+        output <-
+          c(
+            output,
+            fail_symbol(),
+            "Reproduction error! Variable ",
+            var,
+            " was not defined in this chunk!"
+          )
         next
       }
       current_value <- get(var, envir = current_env)
@@ -92,11 +129,17 @@ reproducibleR <- function(options) {
         current_value <- hash(current_value)
       }
 
-      if (is.numeric(current_value)) current_value <- round(current_value,default_digits())
-      if (is.numeric(original_value)) original_value <- round(original_value,default_digits())
+      if (is.numeric(current_value))
+        current_value <- round(current_value, default_digits())
+      if (is.numeric(original_value))
+        original_value <- round(original_value, default_digits())
 
       if (base::identical(original_value, current_value)) {
-        result <- paste0("- ",ok_symbol(output_format),var, ": REPRODUCTION SUCCESSFUL")
+        result <-
+          paste0("- ",
+                 ok_symbol(output_format),
+                 var,
+                 ": REPRODUCTION SUCCESSFUL")
       } else {
         err_counter = err_counter + 1
         if (isTRUE(default_hashing())) {
@@ -105,60 +148,100 @@ reproducibleR <- function(options) {
           errmsg <- "Objects are not identical."
         }
 
-        if (is.character(original_value) && is.character(current_value)) {
-          errmsg <- paste0("Character vectors differ: ",original_value, " vs ", current_value)
+        if (is.character(original_value) &&
+            is.character(current_value)) {
+          errmsg <-
+            paste0("Character vectors differ: ",
+                   original_value,
+                   " vs ",
+                   current_value)
         }
 
         # generate more informative error message for numeric values
-        if (is.numeric(original_value) && is.numeric(current_value))         {
-          if (length(original_value)==1 && length(current_value==1)) {
-            errmsg <- paste0("Numbers are not identical: ",original_value," vs ", current_value, collapse="")
+        if (is.numeric(original_value) &&
+            is.numeric(current_value))         {
+          if (length(original_value) == 1 && length(current_value == 1)) {
+            errmsg <-
+              paste0(
+                "Numbers are not identical: ",
+                original_value,
+                " vs ",
+                current_value,
+                collapse = ""
+              )
           } else {
-            num_diff = sum(original_value!=current_value)
-            errmsg <- paste0(num_diff , " numbers out of ",length(original_value)," differ!\nOriginal values:",paste0(original_value,collapse=", "),"\n",
-                             "Current values: ",
-                             paste0(current_value,collapse=", "))
+            num_diff = sum(original_value != current_value)
+            errmsg <-
+              paste0(
+                num_diff ,
+                " numbers out of ",
+                length(original_value),
+                " differ!\nOriginal values:",
+                paste0(original_value, collapse = ", "),
+                "\n",
+                "Current values: ",
+                paste0(current_value, collapse = ", ")
+              )
           }
 
         }
-        result <- paste0("- ",fail_symbol(output_format),var, ": **REPRODUCTION FAILED** ",errmsg)
+        result <-
+          paste0("- ",
+                 fail_symbol(output_format),
+                 var,
+                 ": **REPRODUCTION FAILED** ",
+                 errmsg)
       }
-      output <- c(output, result,"\n")
+      output <- c(output, result, "\n")
     }
   }
 
   # update error counter
-  num_errors_total = get(x = "repror_error_counter", envir=knitr::knit_global())
+  num_errors_total = get(x = "repror_error_counter", envir = knitr::knit_global())
   num_errors_total = num_errors_total + err_counter
-  assign(x="repror_error_counter",value = num_errors_total, envir=knitr::knit_global())
+  assign(x = "repror_error_counter",
+         value = num_errors_total,
+         envir = knitr::knit_global())
 
-  out <- paste0(output,sep="",collapse="\n")
+  out <- paste0(output, sep = "", collapse = "\n")
   title <- "Code Chunk Reproduction Report"
   code <- options$code
 
   options$results <- "asis"
 
   # write a separate report file
-  if (!is.null(options$report))
-    if (isTRUE(options$report)) {
-
-      if (is.null(this_filename)) this_filename <- ""
-      filename <- paste0("reproducibility_report_",this_filename,"-",label,".txt")
+  if (!is.null(options$reportfile))
+    if (isTRUE(options$reportfile)) {
+      if (is.null(this_filename))
+        this_filename <- ""
+      filename <-
+        paste0("reproducibility_report_",
+               this_filename,
+               "-",
+               label,
+               ".txt")
       con <- file(filename)
-      writeLines(text = out, con=con)
+      writeLines(text = out, con = con)
       close(con)
     }
 
+  if (!is.null(options$report))
+    if (isTRUE(options$report)) {
+      template <- default_templates()
+      if (hasName(template, output_format)) {
+        out <-
+          gsub(pattern = "(\\$\\{output\\})",
+               replacement = out,
+               x = template[[output_format]])
+        out <- gsub(pattern = "(\\$\\{title\\})",
+                    replacement = title,
+                    x = out)
 
-  template <- default_templates()
-  if (hasName(template, output_format)) {
-    out <- gsub(pattern="(\\$\\{output\\})", replacement=out,x=template[[output_format]])
-    out <- gsub(pattern="(\\$\\{title\\})", replacement=title,x=out)
-
-  } else {
-    out <- paste0(paste0("###",title,collapse=""), out,"\n\n",collapse="\n")
-  }
-
+      } else {
+        out <-
+          paste0(paste0("###", title, collapse = ""), out, "\n\n", collapse = "\n")
+      }
+    }
 
 
   knitr::engine_output(options, code, out)
