5b8ae9a58983baa0e5320425d1c35f292b5e654e
brandmaier <brandmaier@mpib-berlin.mpg.de>
2025-01-09 15:31:49 +0100

    added Rd and cleaned up zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,27 @@
 
-.onLoad <- function(libname, pkgname){
-  assign(x="repror_error_counter",value = 0, envir=knitr::knit_global())
-  assign(x="repror_summary",value = data.frame(chunk_name=NULL, variable_name=NULL, success=NULL), envir=knitr::knit_global())
+
+.onLoad <- function(libname, pkgname) {
+  assign(x = "repror_error_counter",
+         value = 0,
+         envir = knitr::knit_global())
+  assign(
+    x = "repror_summary",
+    value = data.frame(
+      chunk_name = NULL,
+      variable_name = NULL,
+      success = NULL
+    ),
+    envir = knitr::knit_global()
+  )
 
   knitr::knit_engines$set(reproducibleR = reproducibleR)
   op <- options()
-  op_add <- list(reproducibleRchunks.filetype = "json",
-                 reproducibleRchunks.digits = 8)
+  op_add <- list(
+    reproducibleRchunks.filetype = "json",
+    reproducibleRchunks.digits = 8
+  )
   toset <- !(names(op_add) %in% names(op))
-  if(any(toset)) options(op_add[toset])
+  if (any(toset))
+    options(op_add[toset])
   invisible()
 }
