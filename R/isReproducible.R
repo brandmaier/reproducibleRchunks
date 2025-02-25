f845869877f904b3d40fb5b83c0905716b8fef35
brandmaier <brandmaier@mpib-berlin.mpg.de>
2025-02-25 17:44:14 +0100

    trying some stuff with environments

--- R/isReproducible.R
+++ R/isReproducible.R
@@ -4,18 +4,20 @@
 #' @param filename An R Markdown file to check for reproducibility
 #' @param \ldots Optional arguments passed down to rmarkdown::render()
 #'
-isReproducible <- function(filename,
+isReproducible <- function(filename, env = knitr::knit_global(),
                            ...)
 {
   if (!file.exists(filename)) stop("File does not exist")
   if (!endsWith(tolower(filename),"rmd")) warning("Possibly not an Rmd file")
 
-  temp_envir <- knitr::knit_global()
+  # this is R's global environment by default
+  #temp_envir <- knitr::knit_global()
 
-  .reset(temp_envir)
+  .reset(env)
 
   rmarkdown::render(input = filename,
-                    envir = temp_envir,
+                    envir = env,
+                    quiet = TRUE,
                     ...)
 
   if (nrow(get_reproducibility_summary()) == 0) {
