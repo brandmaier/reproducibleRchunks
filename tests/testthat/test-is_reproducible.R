c528abd2fd4415fb8427fc524152aaed3cf97cce
Andreas Brandmaier <andy@brandmaier.de>
2025-07-03 09:18:42 +0200

    Add test for isReproducible

---
+++ tests/testthat/test-is_reproducible.R
@@ -0,0 +1,16 @@
+test_that("isReproducible returns TRUE on test_json.Rmd", {
+  rmd <- testthat::test_path("testdata", "test_json.Rmd")
+  html <- testthat::test_path("testdata", "test_json.html")
+  json <- testthat::test_path("testdata", ".repro_test_json.Rmd_somenumbers.json")
+
+  if (file.exists(html)) file.remove(html)
+  if (file.exists(json)) file.remove(json)
+
+  on.exit({
+    if (file.exists(html)) file.remove(html)
+    if (file.exists(json)) file.remove(json)
+  }, add = TRUE)
+
+  res <- reproducibleRchunks::isReproducible(rmd)
+  expect_true(res)
+})
