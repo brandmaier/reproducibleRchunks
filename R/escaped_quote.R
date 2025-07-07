591e0c32062a6c1be687706cca98b097d0033a84
Andreas Brandmaier <andy@brandmaier.de>
2025-07-07 09:23:54 +0200

    docs: clarify escape helper

--- R/escaped_quote.R
+++ R/escaped_quote.R
@@ -1,4 +1,18 @@
-escapedQuote <- function(x)
-{
-  return(gsub('"', '\\\\"', shQuote(x, type = "cmd")))
+#' Escape quotes for shell commands
+#'
+#' This helper prepares character vectors for use in shell commands by
+#' quoting them with \code{shQuote()} and double escaping any internal
+#' double quotes.
+#'
+#' @param x A character vector that may contain double quotes.
+#'
+#' @return A character vector with all double quotes double escaped so
+#'   that it can safely be passed to a command line call.
+#'
+#' @examples
+#' escapedQuote('foo "bar" baz')
+#'
+#' @keywords internal
+escapedQuote <- function(x) {
+  gsub('"', '\\\\"', shQuote(x, type = "cmd"))
 }
